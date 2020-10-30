f473958266ba7a16a953cf462280bcf73121c136
ElderVivot <eldervivot@gmail.com>
2020-10-30 06:20:42 -0300

    feat: update scraping according new site goiania

--- src/scrapings/nfsegoiania/GotoLinkNFeEletrotinaEntrar.ts
+++ src/scrapings/nfsegoiania/GotoLinkNFeEletrotinaEntrar.ts
@@ -5,10 +5,10 @@ import TreatsMessageLog from './TreatsMessageLog'
 
 const GotoLinkNFeEletrotinaEntrar = async (page: Page, settings: ISettingsGoiania): Promise<void> => {
     try {
-        await page.waitFor('#GoianiaTheme_wtTelaPrincipal_block_wtMainContent_WebPatterns_wt160_block_wtContent1_wt57_WebPatterns_wt72_block_wtContent_wt300')
+        await page.waitFor('#GoianiaTheme_wtTelaPrincipal_block_wtMainContent_WebPatterns_wt149_block_wtContent1_wt52_WebPatterns_wt66_block_wtContent_wt296')
 
         const urlButtonEntrar = await page.evaluate(
-            () => document.querySelector('#GoianiaTheme_wtTelaPrincipal_block_wtMainContent_WebPatterns_wt160_block_wtContent1_wt57_WebPatterns_wt72_block_wtContent_wt300')?.getAttribute('href')
+            () => document.querySelector('#GoianiaTheme_wtTelaPrincipal_block_wtMainContent_WebPatterns_wt149_block_wtContent1_wt52_WebPatterns_wt66_block_wtContent_wt296')?.getAttribute('href')
         )
         if (urlButtonEntrar) {
             page.on('dialog', async dialog => {
